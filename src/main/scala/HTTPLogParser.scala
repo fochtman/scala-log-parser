fb41b507268049bb6a3af5b172abb3f68e4bb507
Tyler Fochtman <tlfochtman@gmail.com>
2015-04-27 04:09:26 -0500

    cleaning

--- src/main/scala/HTTPLogParser.scala
+++ src/main/scala/HTTPLogParser.scala
@@ -3,37 +3,34 @@ import org.parboiled2._
 object HTTPLogParser {
 
   sealed trait Log
-  case class Host(s: String) extends Log
-  case class Rfc(s: String) extends Log
-  case class UserName(s: String) extends Log
-  case class Date(dmy: String, hms: String, zone: String) extends Log
-  case class Request(m: String, r: String, p: String) extends Log
-  case class Request0(m: String, r: String) extends Log
-  case class StatusCode(s: String) extends Log
-  case class Bytes(s: String) extends Log
+  case class NCSACommonLog (
+    host: String,
+    rfc: String,
+    userName: String,
+    date: String,
+    method: String,
+    resource: String,
+    statusCode: String,
+    bytes: String
+  ) extends Log
 
   class NCSA(val input: ParserInput) extends Parser {
     def InputLine = rule {
       Expression ~ EOI
     }
-
-    def Expression: Rule1[List[Log]] = rule {
-      host ~ rfc ~ userName ~ date ~ request ~ statusCode ~ bytes ~> (List(_: Host, _: Rfc, _: UserName, _: Date, _: Request0, _: StatusCode, _: Bytes))
-      //host ~ rfc ~ userName ~ date ~ request ~ statusCode ~ bytes ~> ((h: Host, r: Rfc, u: UserName, d: Date, rq: Request, sc: StatusCode, b: Bytes) => List(h, r, u, d, rq, sc, b))
+    def Expression: Rule1[NCSACommonLog] = rule {
+      (Host ~ Rfc ~ UserName ~ Date ~ Method ~ Request ~ StatusCode ~ Bytes) ~>
+        ((h: String, r: String, u: String, dmy: String, hms: String, zone: String, m: String, rq: String, sc: String, b: String) =>
+          NCSACommonLog(h, r, u, dmy+hms+zone, m, rq, sc, b))
     }
 
     /** Host, Start **/
-    def host: Rule1[Host]        = rule { SubDomain ~> (Host(_)) ~ ws }
-    //def host: Rule1[Host]        = rule { (IP | SubDomain) ~> (Host(_)) ~ ws }
-    def IP: Rule1[String]        = rule { capture(oneOrMore(oneOrMore(Digits)).separatedBy('.')) }
-    def SubDomain: Rule1[String] = rule { capture(oneOrMore(URLChars)) }
-    //def SubDomain: Rule1[String] = rule { capture(oneOrMore(oneOrMore(AlphaNums).separatedBy(anyOf(".-_")))) }
-
-    def rfc: Rule1[Rfc]            = rule { capture('-') ~> (Rfc(_)) ~ ws }
-    def userName: Rule1[UserName]  = rule { capture('-') ~> (UserName(_)) ~ ws }
+    def Host      = rule { capture(oneOrMore(URLChars)) ~ ws }
+    def Rfc       = rule { capture('-') ~ ws }
+    def UserName  = rule { capture('-') ~ ws }
 
     /** Date, Start **/
-    def date      = rule { '[' ~ (DMY ~ HMS ~ Zone ~> (Date(_, _, _))) ~ ']' ~ ws }
+    def Date      = rule { '[' ~ DMY ~ HMS ~ Zone ~ ']' ~ ws }
     def DMY       = rule { capture(Day ~ '/' ~ Month ~ '/' ~ Year) }
     def Day       = rule { twoDigits }
     def Month     = rule { (1 to 3).times(CharPredicate.Alpha) }
@@ -46,17 +43,13 @@ object HTTPLogParser {
     /** Date, End **/
 
     /** HTTP Request, Start **/
-    def request    = rule { '"' ~ (Method ~ Resource ~> (Request0(_, _))) ~ ws } //~ '"' ~ ws }
-    //def request    = rule { '"' ~ (Method ~ Resource ~ Protocol ~> (Request(_, _, _))) ~ ws } //~ '"' ~ ws }
-    def Method     = rule { capture("GET" | "POST" | "HEAD" | "PUT" | "DELETE" | "TRACE" | "CONNECT") ~ ws }
-    //def Resource   = rule { capture(oneOrMore(URLChars)) ~ '"' }
-    //def Protocol   = rule { optional('/') ~ oneOrMore(ws) ~ capture("HTTP/" ~ ("1.0" | "V1.0")) ~ '"' | capture('"') | ws ~ capture('"') }
-    def Resource   = rule { capture(oneOrMore(oneOrMore(URLChars)).separatedBy(oneOrMore(ws)) | oneOrMore(URLChars) ~ ws) ~ '"' }
-    def Protocol   = rule { optional('/') ~ oneOrMore(ws) ~ capture("HTTP/" ~ ("1.0" | "V1.0")) ~ '"' | capture('"') | ws ~ capture('"') }
-    /** HTTP Request, End **/
+    def Request    = rule { Resource ~ ws }
+    def Method     = rule { '"' ~ capture("GET" | "POST" | "HEAD" | "PUT" | "DELETE" | "TRACE" | "CONNECT") ~ ws }
+    def Resource   = rule { capture(ResType0) ~ '"' }
+    def ResType0   = rule { oneOrMore(oneOrMore(URLChars)).separatedBy(oneOrMore(ws)) }
 
-    def statusCode = rule { capture((ch('1') | ch('2') | ch('3') | ch('4') | '5') ~ twoDigits) ~> (StatusCode(_)) ~ ws }
-    def bytes      = rule { capture('-' | oneOrMore(Digits)) ~> (Bytes(_)) }
+    def StatusCode = rule { capture((ch('1') | ch('2') | ch('3') | ch('4') | '5') ~ twoDigits) ~ ws }
+    def Bytes      = rule { capture('-' | oneOrMore(Digits)) }
 
     def ws          = rule { CharPredicate(' ') }
     def Digits     = rule { CharPredicate.Digit }
@@ -64,5 +57,8 @@ object HTTPLogParser {
     def AlphaNums  = rule { CharPredicate.AlphaNum }
     def twoDigits  = rule { (1 to 2).times(Digits) }
     def fourDigits = rule { (1 to 4).times(Digits) }
+
+    //def Protocol   = rule { optional('/') ~ oneOrMore(ws) ~ capture("HTTP/" ~ ("1.0" | "V1.0")) ~ '"' | capture('"') | ws ~ capture('"') }
+    /** HTTP Request, End **/
   }
 }
