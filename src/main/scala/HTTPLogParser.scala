8e92130e4352f7d38dc42958798ed81900e613cd
Tyler Fochtman <tlfochtman@gmail.com>
2015-04-27 23:54:31 -0500

    regex & peg parser update

--- src/main/scala/HTTPLogParser.scala
+++ src/main/scala/HTTPLogParser.scala
@@ -1,6 +1,14 @@
+import com.mongodb.casbah.Imports._
 import org.parboiled2._
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 object HTTPLogParser {
+
+  // TODO: Add methods to covert bytes to null or Int, same with statusCode
+  // import org.joda.time.format.ISODateTimeFormat
+  // TODO: convert date to ISODate
+  // TODO: update pegFullNCSA with latest knowledge about PEG efficiency
 
   sealed trait Log
   case class NCSACommonLog (
@@ -11,13 +19,54 @@ object HTTPLogParser {
     method: String,
     resource: String,
     statusCode: String,
-    bytes: String
+    bytes: String,
+    id: ObjectId = new ObjectId()
   ) extends Log
 
-  class NCSA(val input: ParserInput) extends Parser {
-    def InputLine = rule {
+  object NCSACommonLog {
+    def toBson(log: NCSACommonLog): DBObject = {
+      MongoDBObject(
+        "host" -> log.host,
+        "rfc" -> log.rfc,
+        "userName" -> log.userName,
+        "date" -> log.date,
+        "method" -> log.method,
+        "resource" -> log.resource,
+        "statusCode" -> log.statusCode,
+        "bytes" -> log.bytes,
+        "_id" -> log.id
+      )
+    }
+  }
+
+  class pegPartNCSA(val input: ParserInput) extends Parser {
+    import CharPredicate.{Digit, Alpha}
+    def InputLine: Rule1[NCSACommonLog] = rule {
+      capture(url) ~ ws ~ capture(url) ~ ws ~ capture(url) ~ ws ~
+      '[' ~ capture(digit2 ~ '/' ~ alpha3 ~ '/' ~ digit4 ~ ':' ~  // date ../../....:
+      digit2 ~ ':' ~ digit2 ~ ':' ~ digit2 ~ ws ~                 // time ..:..:..
+      (ch('-') | '+') ~ digit4) ~ ']' ~ ws ~                      // zone (-/+)....
+      '"' ~ capture(url ~ ws ~ url) ~ ws ~                        // request type and resource
+      capture(vis) ~ '"' ~ ws ~                                   //
+      capture(digit3) ~ ws ~                                      // status code
+      capture(oneOrMore(Digit)) ~>                                // bytes
+        ((h: String, r: String, u: String, dateTime: String, m: String, rq: String, sc: String, b: String) =>
+          NCSACommonLog(h, r, u, dateTime, m, rq, sc, b))
+    }
+    val ws = ' '
+    def url    = rule { oneOrMore(!ws ~ ANY) }
+    def vis    = rule { oneOrMore(!ws ~ !'"' ~ ANY) }
+    def alpha3 = rule { Alpha ~ Alpha ~ Alpha }
+    def digit2 = rule { Digit ~ Digit }
+    def digit3 = rule { Digit ~ Digit ~ Digit }
+    def digit4 = rule { Digit ~ Digit ~ Digit ~ Digit }
+  }
+
+  class pegFullNCSA(val input: ParserInput) extends Parser {
+    def InputLine: Rule1[NCSACommonLog] = rule {
       Expression ~ EOI
     }
+
     def Expression: Rule1[NCSACommonLog] = rule {
       (Host ~ Rfc ~ UserName ~ Date ~ Method ~ Request ~ StatusCode ~ Bytes) ~>
         ((h: String, r: String, u: String, dmy: String, hms: String, zone: String, m: String, rq: String, sc: String, b: String) =>
@@ -51,7 +100,7 @@ object HTTPLogParser {
     def StatusCode = rule { capture((ch('1') | ch('2') | ch('3') | ch('4') | '5') ~ twoDigits) ~ ws }
     def Bytes      = rule { capture('-' | oneOrMore(Digits)) }
 
-    def ws          = rule { CharPredicate(' ') }
+    def ws         = rule { CharPredicate(' ') }
     def Digits     = rule { CharPredicate.Digit }
     def URLChars   = rule { CharPredicate.Visible -- '"' }
     def AlphaNums  = rule { CharPredicate.AlphaNum }
@@ -60,5 +109,19 @@ object HTTPLogParser {
 
     //def Protocol   = rule { optional('/') ~ oneOrMore(ws) ~ capture("HTTP/" ~ ("1.0" | "V1.0")) ~ '"' | capture('"') | ws ~ capture('"') }
     /** HTTP Request, End **/
+  }
+
+  /**
+   * Taken from an Apache Spark example
+   */
+                          // 1:IP  2:client 3:user 4:date time                   5:method 6:req 7:proto   8:respcode 9:size
+  val LOG_ENTRY_PATTERN = "^(\\S+) (\\S+) (\\S+) \\[([\\w:/]+\\s[+\\-]\\d{4})\\] \"(\\S+ \\S+) (\\S+)\" (\\d{3}) (\\d+)"
+  val PATTERN = Pattern.compile(LOG_ENTRY_PATTERN)
+  def regexNCSA(input: String): Option[NCSACommonLog] = {
+    val m = PATTERN.matcher(input)
+    if (!m.find())
+      None
+    else
+      Option(NCSACommonLog(m.group(1), m.group(2), m.group(3), m.group(4), m.group(5), m.group(6), m.group(7), m.group(8)))
   }
 }
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,7 @@
 
 import java.nio.charset.CodingErrorAction
+import com.mongodb.casbah.MongoClient
+
 import scala.io.Codec
 
 import HTTPLogParser._
@@ -8,39 +10,92 @@ import io.Codec._
 
 object Main {
 
+  val charSet        = "ISO-8859-1" // ISO Latin Alphabet No. 1, a.k.a. ISO-LATIN-1
+  implicit val codec = Codec(charSet)
+  codec.onMalformedInput(CodingErrorAction.REPLACE)
+  codec.onUnmappableCharacter(CodingErrorAction.REPLACE)
+
   def main(args: Array[String]) : Unit = {
-    val filename = args(0)
-
-    // ISO Latin Alphabet No. 1, a.k.a. ISO-LATIN-1
-    val charSet = "ISO-8859-1"
-    implicit val codec = Codec(charSet)
-    codec.onMalformedInput(CodingErrorAction.REPLACE)
-    codec.onUnmappableCharacter(CodingErrorAction.REPLACE)
-
-    var i = 1
+    val filename       = args(0)
     val lines = scala.io.Source.fromFile(filename)(charSet)
 
-    val finalResult =
-      for {
-        l <- lines.getLines()
-        res = new NCSA(l.trim).InputLine.run()
-      } yield {
-        res match {
-          case Success(expr) =>
-            i += 1
-            1.toByte
+    performParseComparison(filename)
+
+    val mongo = false
+
+    if (mongo){
+      val mongoClient = MongoClient("localhost", 27017)
+      val db          = mongoClient("test-logfiles")
+      val coll        = db("test-coll")
+      coll.drop()
+      lines.getLines() foreach ( line =>
+        new pegFullNCSA(line.trim).InputLine.run() match {
+          case Success(expr: NCSACommonLog) =>
+            coll.insert(NCSACommonLog.toBson(expr))
           case Failure(expr) =>
-            i += 1
-            0.toByte
         }
-      }
-    println("FINISHED PARSING")
+      )
+      println(coll.count())
+    }
+  }
 
-    val count = finalResult.count(_ == 1.toByte)
+  def performParseComparison(filename: String): Unit = {
+    val regex   = true
+    val pegPart = true
+    val peg     = true
 
-    println("# PARSED CORRECTLY: " + count)
-    println("OUT OF TOTAL: " + i)
-    println
-    println(i - count)
+    var goods = 0
+    var fails = 0
+
+    println("Regex")
+    if (regex) {
+      val lines = scala.io.Source.fromFile(filename)(charSet)
+      val then = System.currentTimeMillis()
+      lines.getLines() foreach ( line =>
+        regexNCSA(line.trim) match {
+          case Some(expr: NCSACommonLog) => goods += 1
+          case None => fails += 1
+        })
+      val now = System.currentTimeMillis()
+      report(goods, fails, now - then)
+    }
+
+    goods = 0
+    fails = 0
+    println("\nPEG w/ Regex Accuracy")
+    if (pegPart) {
+      val lines = scala.io.Source.fromFile(filename)(charSet)
+      val then = System.currentTimeMillis()
+      lines.getLines() foreach ( line =>
+        new pegPartNCSA(line.trim).InputLine.run() match {
+          case Success(expr: NCSACommonLog) => goods += 1
+          case Failure(expr) => fails += 1
+        })
+      val now = System.currentTimeMillis()
+      report(goods, fails, now - then)
+    }
+
+    goods = 0
+    fails = 0
+    println("\nPEG w/ More Accuracy")
+    if (peg) {
+      val lines = scala.io.Source.fromFile(filename)(charSet)
+      val then = System.currentTimeMillis()
+      lines.getLines() foreach ( line =>
+        new pegFullNCSA(line.trim).InputLine.run() match {
+          case Success(expr: NCSACommonLog) => goods += 1
+          case Failure(expr) => fails += 1
+        })
+      val now = System.currentTimeMillis()
+      report(goods, fails, now - then)
+    }
+  }
+
+  def report(goods: Int, fails: Int, time: Long): Unit = {
+    println(s"TOTAL : ${goods+fails}")
+    println(s"PARSED: $goods")
+    println(s"FAILED: $fails")
+    println(s"PERCNT: ${fails.toDouble / (goods + fails)}")
+    println(s"TIME  : $time")
   }
 }
