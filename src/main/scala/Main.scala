48c4fee6a7176b840a1ca9d84cbbb2e2523d3cea
Tyler Fochtman <tlfochtman@gmail.com>
2015-04-28 06:24:30 -0500

    added embedded REPL to load files to DB
    inspired by:
    https://github.com/mongodb/casbah/blob/master/examples/mongoimport.scala
    and
    https://github.com/sirthias/parboiled2/blob/master/examples/src/main/scala/org/parboiled2/examples/Calculator1.scala

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,55 +1,147 @@
 
+import HTTPLogParser._
 import java.nio.charset.CodingErrorAction
 import com.mongodb.casbah.MongoClient
-
-import scala.io.Codec
-
-import HTTPLogParser._
-import util.{Success, Failure}
+import com.mongodb.casbah.Imports._
 import io.Codec._
+import scala.io.{Source, Codec, StdIn}
+import scala.util.{Success, Failure}
+import scala.annotation.tailrec
+import scala.concurrent._
+import java.io.{ByteArrayOutputStream, PrintStream}
+import scala.Some
+import scala.collection.JavaConverters._
+import com.mongodb.util.JSON
+
 
 object Main {
+  val usage = """
+    |
+    |Example:
+    |  mongoimport --uri mongodb://localhost/my_db.my_collection
+    |
+    |Options:
+    |  --help                                produce help message
+    |  --uri arg                             The connection URI - must contain a collection
+    |                                        mongodb://[username:password@]host1[:port1][,host2[:port2]]/database.collection[?options]""".stripMargin
+
+  val alisDB = "access_log_invariant_search"
+  val alco   = "access_log_collection"
+  case class Options(uri: Option[String] = Some(s"mongodb://localhost:27017/$alisDB.$alco"))
 
   val charSet        = "ISO-8859-1" // ISO Latin Alphabet No. 1, a.k.a. ISO-LATIN-1
   implicit val codec = Codec(charSet)
   codec.onMalformedInput(CodingErrorAction.REPLACE)
   codec.onUnmappableCharacter(CodingErrorAction.REPLACE)
 
+
   def main(args: Array[String]) : Unit = {
-    val filename       = args(0)
-    val lines = scala.io.Source.fromFile(filename)(charSet)
 
-    performParseComparison(filename)
+    if (args.length == 0 | args.contains("--help")) {
+      Console.err.println(usage)
+      sys.exit(1)
+    }
 
-    val mongo = false
+    val optionMap = parseArgs(Map(), args.toList)
+    val options = getOptions(optionMap)
 
-    if (mongo){
-      val mongoClient = MongoClient("localhost", 27017)
-      val db          = mongoClient("test-logfiles")
-      val coll        = db("test-coll")
-      coll.drop()
-      lines.getLines() foreach ( line =>
-        new pegFullNCSA(line.trim).InputLine.run() match {
-          case Success(expr: NCSACommonLog) =>
-            coll.insert(NCSACommonLog.toBson(expr))
-          case Failure(expr) =>
-        }
-      )
-      println(coll.count())
+    val mongoClientURI = MongoClientURI(options.uri.get)
+
+    println(mongoClientURI)
+    println(mongoClientURI.database.get)
+    println(mongoClientURI.collection.get)
+
+    val mc = MongoClient(mongoClientURI)
+    val db = mc(mongoClientURI.database.get)
+    val co = db(mongoClientURI.collection.get)
+    importAccessLogs(co, options)
+  }
+
+  private def parseArgs(map: Map[String, Any], args: List[String]): Map[String, Any] = {
+    args match {
+      case Nil => map
+      //case "--uri" :: value :: tail =>
+      case "--uri" :: value =>
+        val uriStr: Option[String] =
+          if (value.head == "default") Options().uri
+          else Some(value.head)
+        parseArgs(map ++ Map("uri" -> uriStr), Nil)
+      case option :: tail =>
+        Console.err.println("Unknown option " + option)
+        Console.err.println(usage)
+        sys.exit(1)
     }
   }
 
-  def performParseComparison(filename: String): Unit = {
-    val regex   = true
+  private def getOptions(optionMap: Map[String, _]): Options = {
+    /*
+    val default = Options()
+    Options(
+      uri = optionMap.get("uri") match {
+        case None => default.uri
+        case Some(value: String) => Some(value.asInstanceOf[String])
+        //case Some(value) => Some(value.asInstanceOf[String])
+      }
+    )
+    */
+    Options()
+  }
+
+  private def spinWheel(someFuture: Future[_]) {
+    // Let the user know something is happening until futureOutput isCompleted
+    val spinChars = List("!", "/", "$", "\\")
+    while (!someFuture.isCompleted) {
+      spinChars.foreach({
+        case char =>
+          Console.err.print(char)
+          Thread sleep 200
+          Console.err.print("\b")
+      })
+    }
+    Console.err.println("")
+  }
+
+  private def importAccessLogs(co: MongoCollection, options: Options) {
+    def unBuilder = co.initializeUnorderedBulkOperation
+    val batchSize = 1000
+
+    def batchInsert(filename: String): Unit = {
+      val a      = System.currentTimeMillis()
+      val lns    = Source.fromFile(filename)(charSet).getLines()
+      while (lns.hasNext) {
+        val bldr = unBuilder
+        lns.take(batchSize) foreach (line =>
+          new pegPartNCSA(line.trim).InputLine.run() match {
+            case Success(e) => bldr.insert(NCSACommonLog.toBSONSmall(e))
+            case _ => // aka Failure
+          })
+        bldr.execute()
+      }
+      val z      = System.currentTimeMillis()
+      println(s"Inserted ${co.count()} HTTP access logs from $filename in ${a - z} milliseconds.")
+    }
+
+    REPL()
+    @tailrec
+    def REPL(): Unit  = {
+      StdIn.readLine("\nenter filename: ") match {
+        case ""       => // press enter to exit
+        case filename => { batchInsert(filename); REPL() }
+      }
+    }
+  }
+
+  private def performParseComparison(filename: String): Unit = {
+    val regex   = false
     val pegPart = true
-    val peg     = true
+    val peg     = false
 
     var goods = 0
     var fails = 0
 
     println("Regex")
     if (regex) {
-      val lines = scala.io.Source.fromFile(filename)(charSet)
+      val lines = Source.fromFile(filename)(charSet)
       val then = System.currentTimeMillis()
       lines.getLines() foreach ( line =>
         regexNCSA(line.trim) match {
@@ -64,11 +156,13 @@ object Main {
     fails = 0
     println("\nPEG w/ Regex Accuracy")
     if (pegPart) {
-      val lines = scala.io.Source.fromFile(filename)(charSet)
+      val lines = Source.fromFile(filename)(charSet)
       val then = System.currentTimeMillis()
       lines.getLines() foreach ( line =>
         new pegPartNCSA(line.trim).InputLine.run() match {
-          case Success(expr: NCSACommonLog) => goods += 1
+          case Success(expr: NCSACommonLog) =>
+            goods += 1
+            if (goods == 1) println(expr)
           case Failure(expr) => fails += 1
         })
       val now = System.currentTimeMillis()
@@ -79,7 +173,7 @@ object Main {
     fails = 0
     println("\nPEG w/ More Accuracy")
     if (peg) {
-      val lines = scala.io.Source.fromFile(filename)(charSet)
+      val lines = Source.fromFile(filename)(charSet)
       val then = System.currentTimeMillis()
       lines.getLines() foreach ( line =>
         new pegFullNCSA(line.trim).InputLine.run() match {
@@ -91,7 +185,7 @@ object Main {
     }
   }
 
-  def report(goods: Int, fails: Int, time: Long): Unit = {
+  private def report(goods: Int, fails: Int, time: Long): Unit = {
     println(s"TOTAL : ${goods+fails}")
     println(s"PARSED: $goods")
     println(s"FAILED: $fails")
